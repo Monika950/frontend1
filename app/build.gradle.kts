a938560237020c98f5b78b340d7deb5a241ff00e
Monika Georgieva <monikageorgieva950@gmail.com>
2026-06-03 00:33:46 +0300

    Implement encrypted token storage using EncryptedSharedPreferences

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -116,5 +116,7 @@ dependencies {
     implementation(libs.play.services.maps)
     implementation(libs.play.services.location)
     implementation(libs.maps.compose)
+    
+    implementation(libs.security.crypto)
 
 }
